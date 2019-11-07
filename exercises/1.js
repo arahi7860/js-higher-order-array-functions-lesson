4ef2c6b13dd8f75987f26d26952c1cd2b5efef8b
Mikayda Mills <mills.ma12@gmail.com>
2019-11-07 12:25:02 -0500

    remove exercises

--- exercises/1.js
+++
@@ -1,6 +0,0 @@
-// Given an array of fruits, make a new array called `bananas` 
-// with the .filter() method that only contains bananas.
-
-const fruits = ['banana', 'strawberry', 'strawberry', 'banana', 'strawberry'];
-
-console.log('hi');
